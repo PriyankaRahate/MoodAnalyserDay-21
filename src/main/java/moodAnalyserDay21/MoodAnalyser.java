2d4c7ad1373ae2bdbb8cf3772c2bd5f2a172f184
unknown <priyanka.rahate23@gmail.com>
2022-04-16 01:13:13 +0530

    AnalyserSadHappy

--- src/main/java/moodAnalyserDay21/MoodAnalyser.java
+++ src/main/java/moodAnalyserDay21/MoodAnalyser.java
@@ -1,24 +1,16 @@
 package moodAnalyserDay21;
 
 public class MoodAnalyser {
-    private String msg;
-
-    public MoodAnalyser() {
-    }
-
-    public MoodAnalyser(String msg){
-        this.msg = msg;
-    }
-
-    public String analyseMood() throws MoodAnalysisException{
-        try {
-            if(msg.contains("sad")){
-                return "Sad";
-            }
-            return "Happy";
-        }catch (NullPointerException e){
-            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL, "NULL");
+    public String analyseMood(String msg) {
+        if(msg.contains("sad")){
+            return "Sad";
         }
+        return "Happy";
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        System.out.println(moodAnalyser.analyseMood("I am sad today"));
+        System.out.println(moodAnalyser.analyseMood("I am happy today"));
     }
 }
-
