aa210f4c7578240e33456e98d85cfbabab174758
unknown <priyanka.rahate23@gmail.com>
2022-04-16 01:03:26 +0530

    MessageHappySad

---
+++ src/main/java/moodAnalyserDay21/MoodAnalyser.java
@@ -0,0 +1,24 @@
+package moodAnalyserDay21;
+
+public class MoodAnalyser {
+    private String msg;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String msg){
+        this.msg = msg;
+    }
+
+    public String analyseMood() throws MoodAnalysisException{
+        try {
+            if(msg.contains("sad")){
+                return "Sad";
+            }
+            return "Happy";
+        }catch (NullPointerException e){
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL, "NULL");
+        }
+    }
+}
+
